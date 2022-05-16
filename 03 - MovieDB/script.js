9d3300d189a047a244b0c896a62115a19f4f2f98
kaiocp <kaiocp7@gmail.com>
2022-05-16 18:43:20 -0300

    feat: movies section

--- 03 - MovieDB/script.js
+++ 03 - MovieDB/script.js
@@ -1,22 +1,29 @@
-const body = document.querySelector("body");
+const moviesSection = document.querySelector(".movies");
 
-async function getMovie() {
+let imgUrl = "https://image.tmdb.org/t/p/w500";
+
+async function getMovies() {
     const response = await fetch("http://localhost:3000/api");
     const data = response.json();
 
     return data
 }
 
-let imgUrl = "https://image.tmdb.org/t/p/w500";
-
 async function makeHTML() {
-    const movies = await getMovie();
+    const movies = await getMovies();
 
     movies['results'].forEach((el) => {
-        body.innerHTML += `
-        <div>
-            <p>${el.title}</p>
-            <img src="${imgUrl}${el.poster_path}"></img>
+        moviesSection.innerHTML += `
+        <div class="movies__movie">
+            <img src="${imgUrl}${el.poster_path}" alt="Poster do filme">
+            <div class="movies__movie-description">
+                <h2>${el.title}</h2>
+                <p>${el.original_title}</p>
+                <p>${el.release_date}</p>
+                <p>${el.original_language}</p>
+                <p>${el.vote_average}</p>
+                <p>${el.overview}</p>
+            </div>
         </div>
         `
     });
