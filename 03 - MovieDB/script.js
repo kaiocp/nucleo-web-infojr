676efa7c9cdc5165f50951f514007c0ccbe5a9b6
kaiocp <kaiocp7@gmail.com>
2022-05-18 14:03:34 -0300

    feat, style: movies section mobile

--- 03 - MovieDB/script.js
+++ 03 - MovieDB/script.js
@@ -15,18 +15,24 @@ async function makeHTML() {
     movies['results'].forEach((el) => {
         moviesSection.innerHTML += `
         <div class="movies__movie">
-            <img src="${imgUrl}${el.poster_path}" alt="Poster do filme">
+            <img src="${imgUrl}${el.poster_path}" alt="Poster do filme" class="movie-poster">
             <div class="movies__movie-description">
                 <h2>${el.title}</h2>
-                <p>${el.original_title}</p>
-                <p>${el.release_date}</p>
-                <p>${el.original_language}</p>
-                <p>${el.vote_average}</p>
-                <p>${el.overview}</p>
+                <p class="original-title">${el.original_title}</p>
+                <p>Release: ${el.release_date}</p>
+                <p>Original language: ${el.original_language}</p>
+                <div class="movies__movie-description-avs">
+                    <p>Vote average: ${el.vote_average}</p>
+                    <img src="assets/img/star-svgrepo-com.svg" class="star-svg">
+                </div>
+                <hr class=movies__movie-description-hr>
+                <p class="movies__movie-description-sinopsys">Synopsis: ${el.overview}</p>
             </div>
         </div>
         `
     });
 }
+
+// usar li?
 
 makeHTML();
