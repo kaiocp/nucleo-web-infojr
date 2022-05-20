3a5afd84e34ef857c16f54dc8e88341d7d2a5231
kaiocp <kaiocp7@gmail.com>
2022-05-20 12:25:28 -0300

    refactor: container padding and span tags

--- 03 - MovieDB/assets/js/script.js
+++ 03 - MovieDB/assets/js/script.js
@@ -19,16 +19,16 @@ async function makeHTML() {
             <div class="movies__movie-description">
                 <h2>${el.title}</h2>
                 <p class="original-title">${el.original_title}</p>
-                <p>Release: ${el.release_date}</p>
-                <p>Original language: ${el.original_language}</p>
+                <p><span class="bold">Release</span>: ${el.release_date}</p>
+                <p><span class="bold">Original language</span>: ${el.original_language}</p>
                 <div class="movies__movie-description-avs">
-                    <p>Vote average: ${el.vote_average}</p>
+                    <p><span class="bold">Vote average</span>: ${el.vote_average}</p>
                     <img src="assets/img/star-svgrepo-com.svg" class="star-svg">
                 </div>
                 <div class=movies__movie-description-hr>
                     <hr>
                 </div>
-                <p class="movies__movie-description-sinopsys">Synopsis: ${el.overview}</p>
+                <p class="movies__movie-description-sinopsys"><span class="bold">Synopsis</span>: ${el.overview}</p>
             </div>
         </div>
         `
