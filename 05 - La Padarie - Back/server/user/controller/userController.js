77c8643b05dc524233ccb14d869d5a971398b845
kaiocp <kaiocp7@gmail.com>
2022-05-31 20:08:26 -0300

    feat: all endpoints

--- 05 - La Padarie - Back/server/user/controller/userController.js
+++ 05 - La Padarie - Back/server/user/controller/userController.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const userService = require('../service/userService');
 
 router.get('/', userService.getAllUsers);
+router.get('/:id', userService.getUserById);
 
 router.post('/', userService.createUser);
+
+router.delete('/:id', userService.deleteUser);
+
+
 
 module.exports = router;
--- 05 - La Padarie - Back/server/user/service/userService.js
+++ 05 - La Padarie - Back/server/user/service/userService.js
@@ -20,6 +20,26 @@ exports.getAllUsers = (req, res) => {
     })
 }
 
+exports.getUserById = (req, res) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM user WHERE id = ?', [req.params.id],
+            (error, response) => {
+                if (error) {
+                    conn.release();
+
+                    return res.status(500).send({ error: error })
+                }
+                res.status(200).send({
+                    message: "Exibindo infos do usuário",
+                    response: response
+                })
+            }
+        )
+    })
+}
+
 exports.createUser = (req, res) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) }
@@ -36,6 +56,25 @@ exports.createUser = (req, res) => {
 
                 res.status(201).send({
                     mensagem: 'Usuario criado com sucesso'
+                })
+            }
+        )
+    })
+}
+
+exports.deleteUser = (req, res) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'DELETE FROM user WHERE id = ?', [req.params.id],
+            (error) => {
+                if (error) {
+                    conn.release();
+
+                    return res.status(500).send({ error: error })
+                }
+                res.status(200).send({
+                    message: "Usuario removido com sucesso"
                 })
             }
         )
