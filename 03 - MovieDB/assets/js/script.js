3290a705d60335c1959c6ada15a25a9aecbbb2c5
kaiocp <80350711+kaiocp@users.noreply.github.com>
2022-06-20 22:17:13 -0300

    revert

--- 03 - MovieDB/assets/js/script.js
+++ 03 - MovieDB/assets/js/script.js
@@ -3,7 +3,7 @@ const moviesSection = document.querySelector(".movies");
 let imgUrl = "https://image.tmdb.org/t/p/w500";
 
 async function getMovies() {
-    const response = await fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"); // changed just to host on githubpages, but originaly is localhost:3000/api
+    const response = await fetch("http://localhost:3000/api");
     const data = response.json();
 
     return data
